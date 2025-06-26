43bae0183826cf7c3f507230e95590455f214004
akrati gupta <akratigarg1@gmail.com>
2025-06-27 04:00:00 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let a="akrati";
+let b="kumar";
+let c=a+b;
+console.log(c);
+// string concartination 
+
+// let a = "ashish";
+// let b = "kumar";
+
+// let first = "ashish";
+// let last = "kumar";
+
+// let fulname = first+" "+last;
+
+// console.log(fulname);
+let middle = "singh";
+let c = Ashish ${middle} Kumar
+
+console.log(c)
